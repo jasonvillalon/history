882c406852d433cfcf70c28935017596c1446d84
Jason Villalon <jasonvillalon@gmail.com>
2016-01-03 00:03:23 +0800

    initial commit

---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Name: "History",
+  Description: "History",
+  Repository: "git@github.com:jasonvillalon/history.git",
+  AtomicDeps: [],
+  JSPMDeps: {
+    "npm:history": "^1.17.0"
+  }
+};
