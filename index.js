2af8e6642384edff30e7504b41c8ecaf013902ad
Jason Villalon <jasonvillalon@gmail.com>
2016-02-15 18:31:02 +0800

    fix history for mobile

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import createBrowserHistory from "history/lib/createBrowserHistory"
-const History = createBrowserHistory()
+import createHashHistory from "history/lib/createHashHistory"
+const History = window.cordova ? createHashHistory() : createBrowserHistory()
 export default History
